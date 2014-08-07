508f98f2c1f0468325a329be6a5faa47c7c9eb32
Efrén Reyes <efren.reyes@globant.com>
2014-08-07 10:58:32 -0500

    Added method didRotateFromInterfaceOrientation that makes the frame size equal in both portrait and landscape

--- NoAutoLayout/ViewController.m
+++ NoAutoLayout/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutlet UIView *viewOne;
+@property CGRect viewOneFrame;
 @end
 
 @implementation ViewController
@@ -17,13 +18,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.viewOneFrame = self.viewOne.frame;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//Makes the frame size equal in both portrait and landscape
+-(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+    [super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    if (fromInterfaceOrientation == UIInterfaceOrientationLandscapeLeft || fromInterfaceOrientation == UIInterfaceOrientationLandscapeRight) {
+        self.viewOne.frame = self.viewOneFrame;
+    } else {
+        self.viewOne.frame = CGRectMake(self.viewOne.frame.origin.x, self.viewOne.frame.origin.y, self.viewOneFrame.size.width, self.viewOneFrame.size.height);
+    }
 }
 
 @end
